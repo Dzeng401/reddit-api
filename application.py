d897b84019ba0b211f01f26a9233fc038103f948
Dzeng401 <zengd@usc.edu>
2023-08-16 14:26:05 -0700

    8080

--- application.py
+++ application.py
@@ -29,4 +29,4 @@ with application.app_context():
 if __name__ == "__main__":
     with application.app_context():
         db.create_all()
-    application.run(debug = True, port = 8000)
+    application.run(debug = True, port = 8080)
